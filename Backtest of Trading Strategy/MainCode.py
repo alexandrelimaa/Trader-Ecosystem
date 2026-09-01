abad29d0402427a54ca3e49e394866b7a76732e2
Alexandre Lima <alelimacbm@gmail.com>
2026-09-01 09:53:05 -0300

    End of the backtest, I already strated to translate to english, have left the # and the files

--- Backtest of Trading Strategy/MainCode.py
+++ Backtest of Trading Strategy/MainCode.py
@@ -7,7 +7,7 @@ from datetime import time
 #==== Strategy Paraments ====
 short_ema = 9
 long_ema = 400
-timeframe = 3
+timeframe = '3min'
 session_start = time(9 , 40)
 session_end = time(11 , 15)
 stop_loss = 200
@@ -17,33 +17,11 @@ trailing_stop = 15
 #=====================================
 #Cleaning the data:
 candle_1min = h.clean_data(h.load_data("dados/WINFUT2026-08-01(1min).csv"))
-#ummin = pd.read_csv("dados/WINFUT2026-08-01(1min).csv",
-                   # encoding = 'latin1',
-                    #sep = ';',
-                    #thousands='.',
-                    #decimal = ','
-                   # )
-tick_11 = pd.read_csv("dados/2026-08-11a18 tick a tick.csv",
-                      encoding='latin1', sep=';', thousands='.', decimal=',')
-tick_12 = pd.read_csv('dados/tick_12_08.csv',
-                      encoding='latin1', sep=';', thousands='.', decimal=',')
-# ... um pra cada dia
-
-tickatick = pd.concat([tick_11, tick_12], ignore_index=True)
-    #Unificando data e hora
-
-ummin['datetime']= pd.to_datetime(ummin['Data'] + ' ' + ummin['Hora'], dayfirst = True)
-tickatick['datetime']= pd.to_datetime(tickatick['Data']+ ' '+ tickatick['Hora'], dayfirst = True)
-#criando o candle de 3min
-
-tickatick = tickatick.set_index('datetime')
-tickatick = tickatick.sort_index()
-ummin = ummin.set_index('datetime')
-
+tick_data = h.load_all_files('dados/tick_*.csv')
 
 #=================================
-#fazer um for para juntar os candle no timeframe que quero
-candle_3min = ummin.resample('3min') .agg({
+
+candle = candle_1min.resample(timeframe).agg({
     'Abertura' : 'first',
     'Fechamento' : 'last',
     'Volume' : 'sum',
@@ -52,74 +30,73 @@ candle_3min = ummin.resample('3min') .agg({
 })
 
 #Creating EMA's
-#modificar o nome candle_3min
-candle_3min['short_ema'] = candle_3min['Fechamento'].ewm(span=short_ema, adjust=False).mean()
-candle_3min['long_ema'] = candle_3min['Fechamento'].ewm(span=long_ema, adjust=False).mean()
+candle['short_ema'] = candle['Fechamento'].ewm(span=short_ema, adjust=False).mean()
+candle['long_ema'] = candle['Fechamento'].ewm(span=long_ema, adjust=False).mean()
 
 #Creating entry signal
 condition = [
-    candle_3min['Fechamento'] > candle_3min['long_ema'],
-    candle_3min['Fechamento'] < candle_3min['long_ema'],
-    candle_3min['Fechamento'] == candle_3min['long_ema'],
+    candle['Fechamento'] > candle['long_ema'],
+    candle['Fechamento'] < candle['long_ema'],
+    candle['Fechamento'] == candle['long_ema'],
 ]
 results = [
     'up',
     'down',
     'neutral'
 ]
-candle_3min['trend'] = np.select(condition, results, default = 'undefined')
+candle['trend'] = np.select(condition, results, default = 'undefined')
 condition2 = [
-    candle_3min['trend'] == 'up',
-    candle_3min['trend'] == 'down',
-    candle_3min['trend'] == 'neutral'
+    candle['trend'] == 'up',
+    candle['trend'] == 'down',
+    candle['trend'] == 'neutral'
 ]
 results2 = [
-    (candle_3min['Mínimo'] <= candle_3min['short_ema']) & (candle_3min['short_ema'] <= candle_3min['Máximo']),
-    (candle_3min['Máximo'] >= candle_3min['short_ema']) & (candle_3min['short_ema'] >= candle_3min['Mínimo']),
+    (candle['Mínimo'] <= candle['short_ema']) & (candle['short_ema'] <= candle['Máximo']),
+    (candle['Máximo'] >= candle['short_ema']) & (candle['short_ema'] >= candle['Mínimo']),
     False
 ]
-candle_3min['touched_short_ema'] = np.select(condition2, results2)
+candle['touched_short_ema'] = np.select(condition2, results2)
 #Next Candle
-candle_3min['closed_direction'] = np.where(candle_3min['trend'] == 'up',
-                          candle_3min['Fechamento'] > candle_3min['Abertura'], candle_3min['Fechamento'] < candle_3min['Abertura'])
-candle_3min['touched_previous'] = candle_3min['touched_short_ema'].shift(1)
+candle['closed_direction'] = np.where(candle['trend'] == 'up',
+                          candle['Fechamento'] > candle['Abertura'], candle['Fechamento'] < candle['Abertura'])
+candle['touched_previous'] = candle['touched_short_ema'].shift(1)
 
 #Montar o limite de horario
-candle_3min['within_session'] = (candle_3min.index.time >= time(session_start)) & (candle_3min.index.time <= time(session_end))
-#11:18 não entra
-candle_3min['next_within_session'] = candle_3min['within_session'].shift(-1)
+candle['within_session'] = (candle.index.time >= session_start) & (candle.index.time <= session_end)
+candle['next_within_session'] = candle['within_session'].shift(-1)
 
-candle_3min['buy_signal'] =(
-    ((candle_3min['trend'] == 'up') &
-    (candle_3min['closed_direction'] == True) &
-    (candle_3min['touched_short_ema'] == True) &
-    (candle_3min['within_session'] == True) &
-    (candle_3min['next_within_session'] == True)) |
-    ((candle_3min['trend'] == 'alta') &
-    (candle_3min['closed_direction'] == True) &
-    (candle_3min['touched_previous'] == True) &
-    (candle_3min['within_session'] == True) &
-    (candle_3min['next_within_session'] == True))
+candle['buy_signal'] =(
+    ((candle['trend'] == 'up') &
+    (candle['closed_direction'] == True) &
+    (candle['touched_short_ema'] == True) &
+    (candle['within_session'] == True) &
+    (candle['next_within_session'] == True)) |
+    ((candle['trend'] == 'up') &
+    (candle['closed_direction'] == True) &
+    (candle['touched_previous'] == True) &
+    (candle['within_session'] == True) &
+    (candle['next_within_session'] == True))
 )
-candle_3min['sell_signal'] =(
-    ((candle_3min['trend'] == 'down') &
-    (candle_3min['closed_direction'] == True) &
-    (candle_3min['touched_short_ema'] == True) &
-    (candle_3min['within_session'] == True) &
-    (candle_3min['next_within_session'] == True)) |
-    ((candle_3min['trend'] == 'down') &
-    (candle_3min['closed_direction'] == True) &
-    (candle_3min['touched_previous'] == True) &
-    (candle_3min['within_session'] == True) &
-    (candle_3min['next_within_session'] == True))
+candle['sell_signal'] =(
+    ((candle['trend'] == 'down') &
+    (candle['closed_direction'] == True) &
+    (candle['touched_short_ema'] == True) &
+    (candle['within_session'] == True) &
+    (candle['next_within_session'] == True)) |
+    ((candle['trend'] == 'down') &
+    (candle['closed_direction'] == True) &
+    (candle['touched_previous'] == True) &
+    (candle['within_session'] == True) &
+    (candle['next_within_session'] == True))
 )
 
 #Aplicar loop de entrada
 
 # Variavéis simples
-open_position = False
+position_open = False
 trade_type = None          #buy or sell
-entry_price = candle_3min['Abertura'].shift(-1)
+entry_price =None
+candle['buy_sell_price'] = candle['Abertura'].shift(-1)
 exit_price = None
 best_price = None           #preço mais favoravél desde o começo
 trailing_active = False
@@ -129,70 +106,73 @@ profit = None
 trades = []                   # saving trades
 
 #Loop
-inicio_tick = tickatick.index.min()
-for index, candle in candle_3min.loc[inicio_tick:].iterrows():
-    if not posicao_aberta:
+warmup_start = tick_data.index.min()
+for index, candle_row in candle.loc[warmup_start:].iterrows():
 
-        if horario_saida_anterior is not None and index <= horario_saida_anterior:
+    if not position_open:
+        if last_exit_time is not None and index <= last_exit_time:
             continue
-        if candle['sinal_compra']:
-            posicao_aberta = True
-            tipo_operacao = 'compra'
-            preco_entrada =  candle['preco_entrada_compra_venda']
-            stop_atual = preco_entrada - 200
-            melhor_preco = preco_entrada
 
-        elif candle['sinal_venda']:
-            posicao_aberta = True
-            tipo_operacao = 'venda'
-            preco_entrada = candle['preco_entrada_compra_venda']
-            stop_atual = preco_entrada + 200
-            melhor_preco= preco_entrada
-    if posicao_aberta:
-        index_execucao = index +pd.Timedelta(minutes = 3)
-        ticks_da_operacao = tickatick.loc[index_execucao:]
-        for index_tick, tick in ticks_da_operacao.iterrows():
-            if tipo_operacao == 'compra':
-                if  melhor_preco < tick['Preço']:
-                    melhor_preco = tick['Preço']
-                    if melhor_preco >= (preco_entrada + 65):
-                        trailing_ativo = True
-                        stop_atual = ( melhor_preco - 15 )
-                if tick['Preço'] <= stop_atual:
-                    trailing_ativo = False
-                    preco_saida = tick['Preço']
-                    lucro = preco_saida - preco_entrada
+        if candle_row['buy_signal']:
+            position_open = True
+            trade_type = 'buy'
+            entry_price = candle_row['buy_sell_price']
+            current_stoploss = entry_price - stop_loss
+            best_price = entry_price
+
+        elif candle_row['sell_signal']:
+            position_open = True
+            trade_type = 'sell'
+            entry_price = candle_row['buy_sell_price']
+            current_stoploss = entry_price + stop_loss
+            best_price = entry_price
+
+    if position_open:
+        execution_time = index +pd.Timedelta(timeframe)
+        operation_ticks = tick_data.loc[execution_time:]
+        for index_tick, tick in operation_ticks.iterrows():
+            if trade_type == 'buy':
+                if  best_price < tick['Preço']:
+                    best_price = tick['Preço']
+                    if best_price >= (entry_price + trailing_activation):
+                        trailing_active = True
+                        current_stoploss = ( best_price - trailing_stop )
+                if tick['Preço'] <= current_stoploss:
+                    trailing_active = False
+                    exit_price = tick['Preço']
+                    profit = exit_price - entry_price
                     trades.append({
-                        'tipo' : tipo_operacao,
-                        'preco_entrada' : preco_entrada,
-                        'preco_saida' : preco_saida,
-                        'lucro' : lucro,
-                        'hora_entrada' : index,
-                        'hora_saida' : index_tick,
+                        'trade_type' : trade_type,
+                        'entry_price' : entry_price,
+                        'exit_price' : exit_price,
+                        'profit' : profit,
+                        'entry_time' : index,
+                        'exit_time' : index_tick,
                     })
-                    horario_saida_anterior = index_tick
-                    posicao_aberta = False
-
+                    last_exit_time = index_tick
+                    position_open = False
                     break
-            if tipo_operacao == 'venda':
-                if melhor_preco > tick['Preço']:
-                    melhor_preco = tick['Preço']
-                    if melhor_preco <= (preco_entrada - 65) :
-                        trailing_ativo = True
-                        stop_atual = ( melhor_preco + 15 )
-                if tick['Preço'] >= stop_atual:
-                    trailing_ativo = False
-                    preco_saida = tick['Preço']
-                    lucro = preco_entrada - preco_saida
+            if trade_type == 'sell':
+                if best_price > tick['Preço']:
+                    best_price = tick['Preço']
+                    if best_price <= (entry_price - trailing_activation) :
+                        trailing_active = True
+                        current_stoploss = ( best_price + trailing_stop )
+                if tick['Preço'] >= current_stoploss:
+                    trailing_active = False
+                    exit_price = tick['Preço']
+                    profit = entry_price - exit_price
                     trades.append({
-                        'tipo': tipo_operacao,
-                        'preco_entrada': preco_entrada,
-                        'preco_saida': preco_saida,
-                        'lucro': lucro,
-                        'hora_entrada': index,
-                        'hora_saida': index_tick,
+                        'trade_type': trade_type,
+                        'entry_price': entry_price,
+                        'exit_price': exit_price,
+                        'profit': profit,
+                        'entry_time': index,
+                        'exit_time': index_tick,
                     })
-                    horario_saida_anterior = index_tick
-                    posicao_aberta = False
+                    last_exit_time = index_tick
+                    position_open = False
                     break
 df_trades = pd.DataFrame(trades)
+print(len(df_trades))
+print(df_trades.head())
--- Backtest of Trading Strategy/helpers.py
+++ Backtest of Trading Strategy/helpers.py
@@ -1,16 +1,24 @@
 import pandas as pd
+import glob
 
 def load_data(filename):
-    df = pd.read_csv(filepath,
+    df = pd.read_csv(filename,
                      encoding='latin1',
                      sep=';',
                      thousands='.',
                      decimal=',',
                      )
-
+    return df
 
 def clean_data(df):
-    df['datetime'] = pd.to_datetime(df['Data'] + ' ' + df['Time'], dayfirst = True)
+    df['datetime'] = pd.to_datetime(df['Data'] + ' ' + df['Hora'], dayfirst = True)
     df = df.set_index('datetime')
     df = df.sort_index()
     return df
+
+def load_all_files(pattern):
+    filepaths = glob.glob(pattern)
+    dataframes = [clean_data(load_data(fp)) for fp in filepaths]
+    combined = pd.concat(dataframes)
+    combined = combined.sort_index()
+    return combined
